7559d0bd51e9f12c667713f0ce99db0e41f59ebd
Juan Diego <chucjuan508@gmail.com>
2025-08-01 09:54:42 -0800

    excepts

--- Actividad 12.py
+++ Actividad 12.py
@@ -8,3 +8,10 @@ try:
         cant_nots = int(input("Ingrese la cantidad de notas que desea ingresar"))
         for j in range(cant_nots):
             notas = float(input(f"Ingrese la nota {j+1}"))
+            estudiantes[name] = { "Notas": notas}
+except ValueError:
+    print("Error ingrese numeros validos")
+except TypeError:
+    print("No se puede sumar un entero con un texto")
+except ZeroDivisionError:
+    print("No se puede dividir dentro de 0")
